3d48fc1a24ff7c54316ad1d9f7a3eeda96f43bc5
Alina Prysiazhnuyk <alnkaprisiazhnuyk@gmail.com>
2024-02-07 19:48:45 +0200

    style

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -3,15 +3,27 @@ import { Outlet } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import css from './SharedLayout.module.css';
 
+let activeClassName = {
+  color: '#2196f3',
+};
+
 export const SharedLayout = () => {
   return (
     <section>
       <header>
         <div className={css.container_header}>
-          <Link to="/" className={css.link_header}>
+          <Link
+            to="/"
+            className={css.link_header}
+            // style={({ isActive }) => (isActive ? activeClassName : undefined)}
+          >
             Home
           </Link>
-          <Link to="/movies" className={css.link_header}>
+          <Link
+            to="/movies"
+            className={css.link_header}
+            //style={({ isActive }) => (isActive ? activeClassName : undefined)}
+          >
             Movies
           </Link>
         </div>
