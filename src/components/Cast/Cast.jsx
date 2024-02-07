a764fade77ce57dbedaa2be59250f328ca3a7b3e
Alina Prysiazhnuyk <alnkaprisiazhnuyk@gmail.com>
2024-02-07 17:57:57 +0200

    casts styles

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -2,6 +2,7 @@ import { getCastsDetails } from 'components/Api';
 import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import noImage from '../../no-image.jpeg';
+import css from './Cast.module.css';
 
 const Cast = () => {
   const { movieId } = useParams();
@@ -18,12 +19,13 @@ const Cast = () => {
   }, [movieId]);
 
   return (
-    <>
+    <section className={css.section_casts}>
       {cast.length > 0 ? (
-        <ul>
+        <ul className={css.list}>
           {cast.map(({ id, name, profile_path, character }) => (
-            <li key={id}>
+            <li key={id} className={css.item}>
               <img
+                className={css.img}
                 src={
                   profile_path
                     ? `https://image.tmdb.org/t/p/w200/${profile_path}`
@@ -31,15 +33,15 @@ const Cast = () => {
                 }
                 alt={name}
               />
-              <p>{name}</p>
-              <p>Character: {character}</p>
+              <p className={css.link}>Actor: {name}</p>
+              <p className={css.link}>Character: {character}</p>
             </li>
           ))}
         </ul>
       ) : (
         <p>No information about casts</p>
       )}
-    </>
+    </section>
   );
 };
 
